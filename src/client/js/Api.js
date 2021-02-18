c1fc5d1c61980d7a1d6918014b04163ea8c88e68
mMoukhtar <mohamed.moukhtar@outlook.com>
2021-02-18 18:37:59 +0200

    Rel: Heroku 1.1

--- src/client/js/Api.js
+++ src/client/js/Api.js
@@ -1,10 +1,12 @@
 import * as dateUtil from './Util/dateHelper.js';
+
 //Question:
 // I had to add the below ref in order to be able to use this module with jest
 // I understand the fetch api isn't implemented in node by default
 // But I don't understand why jest needs fetch to pass the test
 // also now since I added node-fetch does this mean I overrided the original fetch api that is valid for client side?
 import fetch from 'node-fetch';
+import { urlencoded } from 'body-parser';
 
 const Api = async (cityName, countryCode, countryName, departureDate) => {
   try {
@@ -42,24 +44,25 @@ const Api = async (cityName, countryCode, countryName, departureDate) => {
 //Question4
 // Wha is the meaning of jsonP?
 const loadGeo = (cityName, countryCode) => {
-  let apiURL = new URL('/geo');
-  apiURL.search = new URLSearchParams({ cityName, countryCode }).toString();
+  let apiURL = encodeURI(`/geo?cityName=${cityName}&countryCode=${countryCode}`);
   return fetch(apiURL)
     .then((response) => response.json())
     .catch((error) => console.log('error', `loadGeo:: ${error}`));
 };
 
 const loadWeather = (lng, lat, days) => {
-  let apiURL = new URL('/weather');
-  apiURL.search = new URLSearchParams({ lng, lat, days }).toString();
+  // let apiURL = new URL('/weather');
+  // apiURL.search = new URLSearchParams({ lng, lat, days }).toString();
+  let apiURL = encodeURI(`/weather?lng=${lng}&lat=${lat}&days${days}`);
   return fetch(apiURL)
     .then((response) => response.json())
     .catch((error) => console.log('error', `loadWeather:: ${error}`));
 };
 
 const loadImage = (searchKey) => {
-  let apiURL = new URL('/image');
-  apiURL.search = new URLSearchParams({ searchKey }).toString();
+  // let apiURL = new URL('/image');
+  // apiURL.search = new URLSearchParams({ searchKey }).toString();
+  let apiURL = encodeURI(`/image?searchKey=${searchKey}`);
   return fetch(apiURL)
     .then((response) => response.json())
     .catch((error) => console.log('error', `loadImage:: ${error}`));
