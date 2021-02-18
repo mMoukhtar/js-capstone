93205829d7a929dafee88291324403b1361640d5
mMoukhtar <mohamed.moukhtar@outlook.com>
2021-02-18 19:04:10 +0200

    REL: Heroku v1.2

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,49 +8,57 @@ const TerserPlugin = require('terser-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
-    entry: './src/client/index.js',
-    output: {
-        libraryTarget: 'var',
-        library: 'Client',
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+  entry: './src/client/index.js',
+  output: {
+    libraryTarget: 'var',
+    library: 'Client',
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  // Production settings
+  mode: 'production',
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  resolve: {
+    // ... rest of the resolve config
+    fallback: {
+      zlib: false,
+      path: false,
+      buffer: false,
     },
-    // Production settings
-    mode: 'production',
-    devtool: 'source-map',
-    optimization: {
-        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
-    },
-    module: {
-        rules: [
-            {
-                test: /.m?js$/,
-                use: 'babel-loader',
-                exclude: /node_module/,
-            },
-            {
-                test: /\.s?css$/,
-                use: [{ loader: MiniCssExtractPlugin.loader }, { loader: 'css-loader' }, { loader: 'sass-loader' }],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './src/client/views/index.html',
-            filename: './index.html',
-        }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false,
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-        }),
-        new WorkboxPlugin.GenerateSW(),
+  },
+  module: {
+    rules: [
+      {
+        test: /.m?js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css$/,
+        use: [{ loader: MiniCssExtractPlugin.loader }, { loader: 'css-loader' }, { loader: 'sass-loader' }],
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/client/views/index.html',
+      filename: './index.html',
+    }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new WorkboxPlugin.GenerateSW(),
+  ],
 };
